7a83aba5ddf75cbdd521867a9a2bc73c73835da3
Jiaying <68321745+Jiaying-e@users.noreply.github.com>
2021-01-28 08:58:22 +1000

    Add files via upload

--- app.js
+++ app.js
@@ -16,6 +16,11 @@ app.get("/passion", function(req, res){
     res.render("passion");
 });
 
-app.listen(process.env.PORT || 3000, function(){
+let port = process.env.PORT;
+if (port == null || port == ""){
+    port = 3000;
+}
+
+app.listen(port, function(){
     console.log("Server started on port 3000");
-})
+});
